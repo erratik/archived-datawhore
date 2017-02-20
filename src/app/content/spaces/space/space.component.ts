8a4d3b883bbfc3aebe04f4f8dc754079eec5aac7
erratik <tay@erratik.ca>
2017-02-20 02:06:16 -0500

    [space updates: icon upload] added single space component for edit

---
+++ src/app/content/spaces/space/space.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {SpaceModel} from '../../../models/space.model';
+import {SpacesService} from '../../../services/spaces.service';
+import {SpaceOauthSettings} from '../../../models/space-settings.model';
+import {FileUploader} from 'ng2-file-upload';
+import {Paths} from '../../../classes/paths.class';
+
+@Component({
+    selector: 'datawhore-space',
+    templateUrl: './space.component.html',
+    styleUrls: ['./space.component.less']
+})
+export class SpaceComponent implements OnInit {
+
+    @Input() protected space: SpaceModel;
+
+    public uploader: FileUploader;
+
+    constructor(private spacesService: SpacesService) {}
+
+    ngOnInit() {
+        this.getSpaceOauthSettings();
+        this.uploader = new FileUploader({url: `${Paths.DATAWHORE_API_URL}/upload/${this.space.name}/space?filename=icon`, itemAlias: 'data[attributes][family_photo]'});
+    }
+
+    private getSpaceOauthSettings(): any {
+
+        this.spacesService.getOauthSettings(this.space.name).subscribe((settingsRetrieved: SpaceOauthSettings) => {
+
+            // this.spaces.filter((space: SpaceModel) => {
+            //   if (space.name === spaceConfig.name) {
+            this.space.oauth = settingsRetrieved;
+
+            this.space.toSpaceSettings({
+                name: this.space.name,
+                modified: settingsRetrieved.modified,
+                oauth: settingsRetrieved
+            });
+
+            //   }
+            //
+            // });
+            // this.isLoadingSettings = false;
+        });
+    }
+
+    protected toggleEditSpace(space: SpaceModel): void {
+        space.inEditMode = !space.inEditMode;
+    }
+
+    protected getSpaceModel(spaceName: string): any {
+        return this.spacesService.getSpace(spaceName).subscribe(space => {
+            console.log(space);
+        });
+    }
+    public updateSpaceSettings(updatedSpace: SpaceModel): any {
+        this.spacesService.updateSpace(updatedSpace).subscribe(updatingSpaceRes => {
+
+            this.space.inEditMode = false;
+            return this.space.toSpaceSettings({});
+        });
+    }
+
+}
